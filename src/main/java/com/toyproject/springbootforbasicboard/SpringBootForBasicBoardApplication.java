31eedae591a4d16243f9b09584cf8a4644a7f74a
KMG <kim3jj@icloud.com>
2024-08-30 09:37:33 +0900

    First Commit

---
+++ src/main/java/com/toyproject/springbootforbasicboard/SpringBootForBasicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.springbootforbasicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForBasicBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForBasicBoardApplication.class, args);
+	}
+
+}
